487fa7aa71b751ebc67a7671a8b02ce33bbc3214
Marcus Poehls <marcus.poehls@gmail.com>
2017-06-30 15:25:59 +0200

    add postmark transport via API key; read API key from process.env

--- utils/mailer.js
+++ utils/mailer.js
@@ -5,30 +5,25 @@ const Path = require('path')
 const Boom = require('boom')
 const When = require('when')
 const Nodemailer = require('nodemailer')
+const PostmarkTransport = require('nodemailer-postmark-transport')
 const htmlToText = require('html-to-text')
-const templates = Path.resolve(__dirname, '..', 'email-templates')
+const Templates = Path.resolve(__dirname, '..', 'email-templates')
 const Handlebars = require('handlebars')
 const Promisify = require('es6-promisify')
 
-const Transporter = Nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+const Transporter = Nodemailer.createTransport(PostmarkTransport({
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
+    apiKey: process.env.POSTMARK_API_KEY
   }
-})
+}))
 
 /**
  * filename: email template name, without ".html" file ending. Email templates are located within "server/email-templates"
  * options: data which will be used to replace the placeholders within the template
  **/
 const prepareTemplate = (filename, options = {}) => {
-  // generate site url from content
-  options.siteUrl = config.baseUrl.replace(/\/$/, '')
-
   return When.promise(function (resolve, reject) {
-    const filePath = Path.resolve(templates, filename)
+    const filePath = Path.resolve(Templates, filename)
 
     Fs.readFile(filePath, 'utf8', (err, fileContent) => {
       if (err) {
