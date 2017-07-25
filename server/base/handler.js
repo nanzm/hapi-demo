caef8f585e111d64d3e351d81b1b11a236970921
Marcus Poehls <marcus.poehls@gmail.com>
2017-07-25 11:25:38 +0200

    remove unnecessary redirect from base routes

--- server/base/handler.js
+++ server/base/handler.js
@@ -4,44 +4,24 @@ const Boom = require('boom')
 
 const Handler = {
   index: {
-    plugins: {
-      'hapi-auth-cookie': {
-        redirectTo: false
-      }
-    },
     handler: function (request, reply) {
       reply.view('index', null, { layout: 'hero' })
     }
   },
 
   css: {
-    plugins: {
-      'hapi-auth-cookie': {
-        redirectTo: false
-      }
-    },
     handler: {
       directory: { path: './public/css' }
     }
   },
 
   js: {
-    plugins: {
-      'hapi-auth-cookie': {
-        redirectTo: false
-      }
-    },
     handler: {
       directory: { path: './public/js' }
     }
   },
 
   images: {
-    plugins: {
-      'hapi-auth-cookie': {
-        redirectTo: false
-      }
-    },
     handler: {
       directory: { path: './public/images' }
     }
